81b7909cac3d280b77b7e7676fc338b2ef5ff577
Pawel Licznerski <pawel.licznerski3@gmail.com>
2017-08-14 08:54:16 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require ("gulp");
+var sass = require ("gulp-sass");
+var sourcemaps = require ("gulp-sourcemaps");
+
+gulp.task("rabarbar", function(){
+  return gulp.src("scss/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("css"))
+})
+gulp.task ("watch", function(){
+  gulp.watch('scss/**/*.scss',["rabarbar"]);
+})
